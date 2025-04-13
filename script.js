b9a44692f63ff6f59f02efedaabc915d4b86009f
Maria Eduarda Araujo <maeduardaaraujo01@gmail.com>
2025-04-13 18:51:57 -0300

    commit6final

--- script.js
+++ script.js
@@ -18,6 +18,32 @@ prevBtn.addEventListener('click', () => {
   current = (current - 1 + banners.length) % banners.length;
   showBanner(current);
 });
-document.getElementById('menu-toggle').addEventListener('click', function () {
-  document.getElementById('nav-menu').classList.toggle('show');
+
+const toggleButton = document.getElementById('menu-toggle');
+const navMenu = document.getElementById('nav-menu');
+const navLinks = navMenu.querySelectorAll('a');
+
+// Abrir/fechar menu
+toggleButton.addEventListener('click', () => {
+  navMenu.classList.toggle('show');
+});
+
+// Fechar menu ao clicar em qualquer link
+navLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    navMenu.classList.remove('show');
+  });
+});
+
+// Função para redirecionar para o WhatsApp com mensagem personalizada
+const botaoComprar = document.querySelectorAll('.btn');
+
+botaoComprar.forEach(botao => {
+  botao.addEventListener('click', function () {
+    const produto = this.parentElement.querySelector('h3').textContent;
+    const numero = '5521964269909';
+    const mensagem = encodeURIComponent(`Olá! Tenho interesse no produto: ${produto}`);
+    const link = `https://wa.me/${numero}?text=${mensagem}`;
+    window.open(link, '_blank');
+  });
 });
